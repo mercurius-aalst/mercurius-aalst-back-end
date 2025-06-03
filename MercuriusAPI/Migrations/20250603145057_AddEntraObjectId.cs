0554264d828ac1bdf8ca7a83ce3600da37dd2060
SvenPepermans <sv3npepermans@gmail.com>
2025-06-03 17:09:29 +0200

    Added migrations

---
+++ MercuriusAPI/Migrations/20250603145057_AddEntraObjectId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MercuriusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntraObjectId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EntraObjectID",
+                table: "Players",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntraObjectID",
+                table: "Players");
+        }
+    }
+}

a397d52f8e711b736c92a9168e1b54ffad0ce664
SvenPepermans <sv3npepermans@gmail.com>
2025-06-10 13:36:22 +0200

    Add TeamInvites to GetTeamDTO
    Only team invites with pending status should ever be returned to clients.

--- src/MercuriusAPI/DTOs/LAN/TeamDTOs/GetTeamDTO.cs
+++ src/MercuriusAPI/DTOs/LAN/TeamDTOs/GetTeamDTO.cs
@@ -9,11 +9,13 @@ namespace MercuriusAPI.DTOs.LAN.TeamDTOs
         public string Name { get; set; }
         public int CaptainId { get; set; }
         public IEnumerable<GetTeamPlayerDTO> Players { get; set; } = [];
+        public IEnumerable<TeamInviteDTO> TeamInvites { get; set; } = [];
         public GetTeamDTO(Team team)
         {
             Id = team.Id;
             Name = team.Name;
             Players = team.Players.Select(p => new GetTeamPlayerDTO(p));
+            TeamInvites = team.TeamInvites.Where(i => i.Status == TeamInviteStatus.Pending).Select(i => new TeamInviteDTO(i));
             CaptainId = team.CaptainId;
         }
     }

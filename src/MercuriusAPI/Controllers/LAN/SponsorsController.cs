a2b6606602909b27d8f4468ec0af239a98c20abf
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2025-11-10 12:38:27 +0100

    chore: further clean up

--- src/MercuriusAPI/Controllers/LAN/SponsorsController.cs
+++ src/MercuriusAPI/Controllers/LAN/SponsorsController.cs
@@ -3,52 +3,51 @@ using MercuriusAPI.Services.LAN.SponsorServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MercuriusAPI.Controllers.LAN
+namespace MercuriusAPI.Controllers.LAN;
+
+[Route("lan/[controller]")]
+[Authorize(Roles = "admin")]
+[ApiController]
+public class SponsorsController
 {
-    [Route("lan/[controller]")]
-    [Authorize(Roles = "admin")]
-    [ApiController]
-    public class SponsorsController
+    private readonly ISponsorService _sponsorService;
+
+    public SponsorsController(ISponsorService sponsorService)
     {
-        private readonly ISponsorService _sponsorService;
-
-        public SponsorsController(ISponsorService sponsorService)
-        {
-            _sponsorService = sponsorService;
-        }
+        _sponsorService = sponsorService;
+    }
 
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IEnumerable<GetSponsorDTO> GetAll()
-        {
-            return _sponsorService.GetSponsors();
-        }
+    [HttpGet]
+    [AllowAnonymous]
+    public IEnumerable<GetSponsorDTO> GetAll()
+    {
+        return _sponsorService.GetSponsors();
+    }
 
 
-        [HttpGet("{id}")]
-        [AllowAnonymous]
-        public Task<GetSponsorDTO> GetByIdAsync(int id)
-        {
-            return _sponsorService.GetSponsorByIdAsync(id);
-        }
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    public Task<GetSponsorDTO> GetByIdAsync(int id)
+    {
+        return _sponsorService.GetSponsorByIdAsync(id);
+    }
 
-        [HttpPost]
-        public Task<GetSponsorDTO> CreateAsync([FromForm] CreateSponsorDTO sponsorDTO)
-        {
-            return _sponsorService.CreateSponsorAsync(sponsorDTO);
-        }
+    [HttpPost]
+    public Task<GetSponsorDTO> CreateAsync([FromForm] CreateSponsorDTO sponsorDTO)
+    {
+        return _sponsorService.CreateSponsorAsync(sponsorDTO);
+    }
 
-        [HttpPatch("{id}")]
-        public Task<GetSponsorDTO> UpdateAsync(int id, [FromForm] UpdateSponsorDTO value)
-        {
-            return _sponsorService.UpdateSponsorAsync(id, value);
-        }
+    [HttpPatch("{id}")]
+    public Task<GetSponsorDTO> UpdateAsync(int id, [FromForm] UpdateSponsorDTO value)
+    {
+        return _sponsorService.UpdateSponsorAsync(id, value);
+    }
 
-        [HttpDelete("{id}")]
-        public Task DeleteAsync(int id)
-        {
-            return _sponsorService.DeleteSponsorAsync(id);
-        }
+    [HttpDelete("{id}")]
+    public Task DeleteAsync(int id)
+    {
+        return _sponsorService.DeleteSponsorAsync(id);
     }
 }
